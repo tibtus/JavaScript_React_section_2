60b1d0c9aabe5abc3ce1b6794579a27dca1aa753
TibTus <tibtus@gmail.com>
2021-09-23 14:08:42 +0300

    #_new_project_creates_files

---
+++ src/js/script.js
@@ -0,0 +1,19 @@
+"use strict";
+
+let number = 5;
+const leftBorderWidth = 1;
+
+number = 20;
+console.log(number);
+
+const obj = {
+    a:50
+};
+
+obj.a = 10;
+
+console.log(obj);
+
+
+console.log(name);
+var name = 'Ivan';
