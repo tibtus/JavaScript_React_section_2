78a66913fe62107320c6f5ed054bf4dd05dce676
TibTus <tibtus@gmail.com>
2021-09-25 17:21:14 +0300

    #Modification of the script.js file

--- src/js/script.js
+++ src/js/script.js
@@ -21,7 +21,7 @@
 
 Проверить, чтобы все работало без ошибок в консоли */
 
-'use strict';
+/* 'use strict';
 
 
 const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
@@ -46,4 +46,39 @@ personalMovieDB.movies[a] = b ;
 personalMovieDB.movies[c] = d ;
 
 
-console.log(personalMovieDB);
+console.log(personalMovieDB); */
+
+
+/* if (4 == 9) {
+    console.log("Ok!");
+
+} else {
+    console.log("NOOOOOO!!!!");
+
+}
+
+const num = 50;
+
+if (num < 49) {
+    console.log('сума больше чем 49');
+} else if (num > 100) {
+    console.log('сума меньше чем  100');
+} else {
+    console.log('сума ' + num);
+}
+
+(num ===50) ? console.log('Да верно') : console.log('НЕТ !!!'); */
+
+
+const num = 50;
+
+switch (num) {
+    case 49: console.log('Неверно');
+    break;
+    case 100: console.log('Неверно');
+    break;
+    case 51: console.log('ВЕРНО!!!');
+    break;
+    default: console.log('Если всё выше не верно');
+    break;
+}
